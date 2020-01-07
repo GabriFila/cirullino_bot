a6c48f06b88515ef5bdc6aed81e48196feca288a
Gabri Fila <gabriele.filaferro@gmail.com>
2020-01-07 14:28:29 +0100

    Added method to try parsing private key for heroku

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV == "dev") {
 admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email: process.env.FIREBASE_CLIENT_EMAIL
   }),
   databaseURL: "https://cirullino-a81df.firebaseio.com"
