b9f3428d4f73e0b9d75daf15839beb3eaa14aab5
Gabri Fila <gabriele.filaferro@gmail.com>
2020-01-08 11:39:19 +0100

    MAde progress, now on to game logic

--- main.js
+++ main.js
@@ -2,16 +2,17 @@
 const Telegraf = require("telegraf");
 const Stage = require("telegraf/stage");
 const session = require("telegraf/session");
-const WizardScene = require("telegraf/scenes/wizard");
 const { Markup } = Telegraf;
 const fetch = require("node-fetch");
 const admin = require("firebase-admin");
 const Scene = require("telegraf/scenes/base");
 const { leave } = Stage;
 
+require("dotenv").config();
+
 //helper function
 const sendToUser = (chatId, text, buttons, columns) => {
-  bot.telegram.sendMessage(
+  return bot.telegram.sendMessage(
     chatId,
     text,
     buttons
@@ -31,13 +32,11 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
 };
 
-const composeGroupName = users => {
+const composeGroupName = usernames => {
   let groupName = "";
-  users.sort().forEach(user => (groupName += `&${user}`));
+  usernames.forEach(user => (groupName += `&${user}`));
   return groupName.substr(1);
 };
-
-require("dotenv").config();
 
 const URL = process.env.URL; // get the Heroku config var URL
 const BOT_TOKEN = process.env.BOT_TOKEN; // get Heroku config var BOT_TOKEN
@@ -68,161 +67,171 @@ const db = admin.firestore();
 
 //create bot
 
-// const saluter = new Scene("saluter");
-// saluter.hears(/ciccia/gi, ctx => ctx.reply("Bella"));
-
-// saluter.enter(ctx => console.log(ctx.session.data));
-// greeter.enter(ctx => ctx.reply("Entrato"));
-// greeter.hears(/spegniti/gi, leave());
-
-// greeter.on("message", ctx => {
-//   ctx.session.data = ctx.message.text;
-//   ctx.scene.enter("saluter");
-// });
-// greeter.leave(ctx => ctx.reply("Bye"));
-
 // Create scene manager
 const stage = new Stage();
-stage.command("cancel", leave());
+//stage.command("cancel", leave());
 
 // Scene registration
 
-const getOpponent = new Scene("get-opponent");
+const getOpponent = new Scene("know-opponent");
 const checkOpponent = new Scene("check-opponent");
+const callOpponent = new Scene("call-opponent");
+const createGroup = new Scene("create-group");
 const prepGame = new Scene("prep-game");
+const showGameState = new Scene("show-game-state");
 const makeMove = new Scene("make-move");
 
 // ANCHOR bot scenes
 
 getOpponent.enter(ctx => {
-  console.info("get-opponent");
-  console.log(ctx.message.text);
+  console.info("know-opponent");
   ctx.reply("Chi vuoi sfidare?");
   ctx.scene.enter("check-opponent");
 });
 
 checkOpponent.on("text", ctx => {
   console.info("check-opponent");
-  console.log(ctx.message.text);
 
   const targetUsername = ctx.message.text.toLowerCase();
 
   const opponentRef = db.collection("users").doc(`${targetUsername}`);
   opponentRef
     .get()
-    .then(doc => {
-      if (doc.exists) {
-        ctx.scene.playersInfo = [
-          { ...doc.data(), username: opponentRef.id },
-          db.collection("users").doc`${ctx.message.from.username.toLowerCase()}`
-        ];
-        console.log(doc.data());
-        ctx.scene.enter("prep-game");
+    .then(opponentDoc => {
+      if (opponentDoc.exists) {
+        let startPlayer = { username: ctx.message.from.username.toLowerCase() };
+        db.collection("users")
+          .doc(`${ctx.message.from.username.toLowerCase()}`)
+          .get()
+          .then(startPlayerDoc => {
+            startPlayer = { ...startPlayer, ...startPlayerDoc.data() };
+            ctx.scene.players = [startPlayer, { ...opponentDoc.data(), username: opponentRef.id }];
+            ctx.scene.enter("call-opponent");
+          });
       } else {
         ctx.reply(`@${ctx.message.text} non si è connesso a cirullino, introltragli questo link`);
         ctx.reply(` http://t.me/cirullino_bot `);
-        return;
       }
     })
     .catch(err => console.error(err));
 });
 
-prepGame.enter(ctx => {
-  ctx.reply(`Controllo che l'utente abbia attivato il gioco...`);
+callOpponent.enter(ctx => {
+  console.info("call-opponent");
 
-  const opponent = ctx.scene.opponent;
-  bot.telegram
-    .sendMessage(opponent.chatId, `${opponent.first_name} sei stato invitato a giocare`)
-    .then(() => {
-      //success in sending message
-      let player0, player1;
-      // if (ctx.message.from.username < targetUsername) {
-      //   player0 = ctx.message.from.username;
-      //   chatId1 = ctx.message.chat.id;
-      //   player1 = targetUsername;
-      //   chatId2 = opponent.chatId;
-      // } else {
-      //   player0 = targetUsername;
-      //   player1 = ctx.message.from.username;
-      // }
-      const newGroup = db.collection("groups").doc(composeGroupName(opponent.username));
+  // TODO check if opponent wants to play
+  // ctx.reply(`Controllo che l'utente abbia attivato il gioco...`);
 
-      newGroup.set({
-        players: {
-          0: player0,
-          1: player1
-        }
+  // bot.telegram
+  //success in sending message
+
+  //   .sendMessage(opponent.chatId, `${opponent.first_name} sei stato invitato a giocare`)
+  //   .then(() => {
+  //     ctx.scene.enter("prep-game");
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //     ctx.reply(`C'è stato un problema, contatta lo sviluppatore`);
+  //   });
+
+  ctx.scene.enter("create-group");
+});
+
+createGroup.enter(ctx => {
+  console.info("create-group");
+  const { players } = ctx.scene;
+  //sort players array based on username
+  players.sort((a, b) => a.username > b.username);
+  const newGroup = db.collection("groups").doc(composeGroupName(players.map(player => player.username)));
+
+  newGroup.set({ players });
+  ctx.scene.newGroup = newGroup;
+  ctx.scene.enter("prep-game");
+});
+
+prepGame.enter(async ctx => {
+  console.info("prep-game");
+
+  const { newGroup } = ctx.scene;
+  const groupGames = newGroup.collection("groupGames");
+
+  db.collection("decks")
+    .doc("40cards")
+    .get()
+    .then(doc => {
+      const shuffledDeck = doc.data().deck.sort(() => Math.random() - 0.5);
+      const newGame = {
+        deck: shuffledDeck,
+        hands: {
+          0: shuffledDeck.splice(0, 3),
+          1: shuffledDeck.splice(0, 3)
+        },
+        board: shuffledDeck.splice(0, 4),
+        points: 0,
+        movesRecord: [],
+        userStrongDeck: {
+          0: [],
+          1: []
+        },
+        userWeakDeck: {
+          0: [],
+          1: []
+        },
+        activeUser: getRandomInt(0, 2)
+      };
+      groupGames.add(newGame).then(() => {
+        ctx.scene.game = newGame;
+        ctx.scene.enter("show-game-state");
       });
-      const groupGames = newGroup.collection("groupGames");
-
-      db.collection("decks")
-        .doc("40cards")
-        .get()
-        .then(doc => {
-          const shuffledDeck = doc.data().deck.sort(() => Math.random() - 0.5);
-          const newGame = {
-            chatIds: [chatId1, chatId1],
-            deck: shuffledDeck,
-            hands: {
-              0: shuffledDeck.splice(0, 3),
-              1: shuffledDeck.splice(0, 3)
-            },
-            board: shuffledDeck.splice(0, 4),
-            points: 0,
-            movesRecord: [],
-            userStrongDeck: {
-              0: [],
-              1: []
-            },
-            userWeakDeck: {
-              0: [],
-              1: []
-            },
-            activeUser: getRandomInt(0, 2)
-          };
-          groupGames.add(newGame).then(() => {
-            ctx.session.game = newGame;
-            ctx.scene.enter("make-move");
-          });
-        })
-        .catch(err => {
-          console.error(err);
-        });
     })
     .catch(err => {
       console.error(err);
-      ctx.reply(`C'è stato un problema, contatta lo sviluppatore`);
     });
-  ctx.reply(`Perfetto! Ora puoi giocare con ${opponent.first_name}`);
+
+  ctx.reply(`Perfetto! Tutto è pronto per giocare`);
 });
 
-makeMove.enter(ctx => {
-  game = ctx.session.game;
-  console.log(ctx.session.game);
+showGameState.enter(ctx => {
+  console.info("make-move");
+  const { game, players } = ctx.scene;
   const messages = [];
 
-  game.chatIds.forEach((id, i) => {
+  players.forEach((player, i) => {
     messages.push(
       `In tavola:   ${cardify(game.board)}\nHai ${game.userStrongDeck[i].length} scope e ${game.userWeakDeck[i].length} carte nel mazzo`
     );
+    console.log(players[i].chatId);
 
-    sendToUser(game.chatIds[i], messages[i]);
+    sendToUser(players[i].chatId, messages[i]).then(() => ctx.scene.enter("make-move"));
   });
-
-  sendToUser(game.chatIds[game.activeUser], "Tocca a te! Gioca!", game.hands[game.activeUser]);
 });
 
+makeMove.enter(ctx => {
+  const { players, game } = ctx.scene;
+  sendToUser(players[game.activeUser].chatId, `${players[game.activeUser].first_name} tocca a te! Gioca!`, game.hands[game.activeUser]);
+});
+
+makeMove.on("text", ctx => {
+  console.log(ctx.message.text);
+  ctx.scene.leave();
+});
+
+makeMove.leave(ctx => ctx.reply("Bye"));
+//add bot scenes
 stage.register(getOpponent);
 stage.register(checkOpponent);
-stage.register(makeMove);
+stage.register(callOpponent);
+stage.register(createGroup);
 stage.register(prepGame);
+stage.register(showGameState);
+stage.register(makeMove);
 
 bot.use(session());
 bot.use(stage.middleware());
 
 // ANCHOR bot commands
 bot.start(ctx => {
-  console.log("/start");
+  console.info("/start");
   ctx.reply("Arrivo! Aspetta ancora un attimo");
   const newUserRef = db.collection("users").doc(`${ctx.message.chat.username.toLowerCase()}`);
 
@@ -244,12 +253,11 @@ bot.start(ctx => {
 
 bot.command("newgame", ctx => {
   console.info("/newgame");
-  ctx.scene.enter("get-opponent");
+  ctx.scene.enter("know-opponent");
 });
 
 bot.command("help", ctx => {
   console.info("/help");
-
   ctx.reply(`Se hai delle necessità su questo bot scrvi una mail a gabriele.filaferro@gmail.com`);
 });
 
@@ -269,3 +277,11 @@ bot.hears("test", ctx => {});
 // ANCHOR hint
 //to have separate buttons in keyboard for cards in hand
 //[["one"], ["two", "three"]]
+// const saluter = new Scene("saluter");
+// saluter.hears(/ciccia/gi, ctx => ctx.reply("Bella"));
+
+// saluter.enter(ctx => console.log(ctx.session.data));
+// greeter.enter(ctx => ctx.reply("Entrato"));
+// greeter.hears(/spegniti/gi, leave());
+
+// greeter.leave(ctx => ctx.reply("Bye"));
