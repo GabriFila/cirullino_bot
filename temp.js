a74536fad4a82b01efcbbd7a3ea09929e7b9aaf1
Gabriele <gabriele.filaferro@gmail.com>
2020-01-14 23:09:30 +0100

    Progresses, ending game know has a bit more sense

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
-strong = { 0: ["3♦️", "2♦️", "A♦️"], 1: ["J♦️", "4♦️"] };
-weak = { 0: ["5♦️", "4♦️", "7♦️"], 1: ["Q♦️", "K♦️"] };
+strong = { 0: ["3♦️", "2♦️", "A♦️"], 1: ["J♦️", "7♦️"] };
+weak = { 0: ["5♦️", "4♦️", "7♥️"], 1: ["Q♦️", "K♦️"] };
 
 const calculatePoints = (strongDecks, weakDecks) => {
   const points = [];
@@ -12,6 +12,7 @@ const calculatePoints = (strongDecks, weakDecks) => {
   let piccolaValue = 0;
   let whoHasGrande = -1; // if someone hasthen it is the player index
   let whoHasSeven = -1; // if someone hasthen it is the player index
+
   // add 'scope' to points
   for (let [key, value] of Object.entries(strongDecks)) {
     points.push(value.length);
@@ -22,10 +23,10 @@ const calculatePoints = (strongDecks, weakDecks) => {
     //count cards
     cards.push(weakDecks[key].length);
     // TODO keep going on primiera
-    if (weakDecks[key].filter(card => card.charAt(0) == 7).length == 3) whoHasPrimiera = key;
+    if (weakDecks[key].filter(card => card.charAt(0) == 7).length == 3) whoHasPrimiera = Number(key);
     //calc piccola
     if (weakDecks[key].includes("A♦️") && weakDecks[key].includes("2♦️") && weakDecks[key].includes("3♦️")) {
-      //   whoHasPiccola = key;
+      whoHasPiccola = Number(key);
       for (let i = 0; i < 3; i++) {
         if (weakDecks[key].includes(`${i + 4}♦️`)) piccolaValue = i + 1;
         else break;
@@ -35,29 +36,31 @@ const calculatePoints = (strongDecks, weakDecks) => {
     }
     //calc grande
     if (weakDecks[key].includes("K♦️") && weakDecks[key].includes("Q♦️") && weakDecks[key].includes("J♦️")) {
-      whoHasGrande = key;
+      whoHasGrande = Number(key);
       points[key] += 5;
     }
 
     //calc settebello
     if (weakDecks[key].includes("7♦️")) {
-      whoHasSeven = key;
+      whoHasSeven = Number(key);
       points[key]++;
     }
   }
+
+  // TODO check equality case in denari and carte
   whoHasDiamonds = indexOfMax(diamonds);
   points[whoHasDiamonds]++;
   whoHasCards = indexOfMax(cards);
   points[whoHasCards]++;
 
-  console.log("diamonds", diamonds);
-  console.log("cards", cards);
-  console.log("piccola", whoHasPiccola);
-  console.log("piccola value", piccolaValue);
-  console.log("grande", whoHasGrande);
-  console.log("sette bello", whoHasSeven);
-  console.log("primiera", whoHasPrimiera);
-  return { points };
+  // console.log("diamonds", diamonds);
+  // console.log("cards", cards);
+  // console.log("piccola", whoHasPiccola);
+  // console.log("piccola value", piccolaValue);
+  // console.log("grande", whoHasGrande);
+  // console.log("sette bello", whoHasSeven);
+  // console.log("primiera", whoHasPrimiera);
+  return { points, whoHasCards, whoHasDiamonds, whoHasPiccola, piccolaValue, whoHasGrande, whoHasSeven, whoHasPrimiera };
 };
 
 console.log(calculatePoints(strong, weak));
